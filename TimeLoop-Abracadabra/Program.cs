cc4a74f64b2cca98011a06b08f7aa1f6a2e4704c
jolar <jolar@JOLA>
2021-09-02 18:52:38 -0400

    Add project files.

---
+++ TimeLoop-Abracadabra/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TimeLoop
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int x = int.Parse(Console.ReadLine());
+
+            if ((1 <= x) && (x <= 100))
+
+                for (int i = 1; i <= x; i++)
+
+                    Console.WriteLine(i + " Abracadabra");
+
+        }
+
+    }
+
+}
